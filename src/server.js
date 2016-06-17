5737e50cfc4ed4e03e01bf1f4018b1969c521c41
Michael Reynolds <michael4reynolds@gmail.com>
2016-06-17 13:14:10 -0400

    use promises instead of events

--- src/server.js
+++ src/server.js
@@ -1,52 +1,68 @@
-import unirest from 'unirest'
-import express from 'express'
-import events from 'events'
+const unirest = require('unirest')
+const express = require('express')
+const path = require('path')
 
 const app = express()
 
+// API Functions
+
 const getFromApi = (endpoint, args) => {
-  const emitter = new events.EventEmitter()
-  unirest.get(`https://api.spotify.com/v1/${endpoint}`)
-    .qs(args)
-    .end(response => {
-      if (response.ok) {
-        emitter.emit('end', response.body)
-      } else {
-        emitter.emit('error', response.code)
-      }
+  return new Promise((resolve, reject) => {
+    unirest.get(`https://api.spotify.com/v1/${endpoint}`)
+      .qs(args)
+      .end(response => {
+        if (response.ok) {
+          resolve(response.body)
+        } else {
+          reject('error', response.code)
+        }
+      })
+  })
+}
+
+const getArtist = (name) => {
+  return getFromApi('search', {q: name, limit: 1, type: 'artist'})
+    .then(response => response.artists.items[0], handleError)
+}
+
+const getRelatedArtists = (artist) => {
+  return getFromApi(`artists/${artist.id}/related-artists`)
+    .then(response => {
+      artist.related = response.artists
+      return artist
+    }, handleError)
+}
+
+const getTopTracks = (mainArtist) => {
+  var promises = mainArtist.related
+    .map(function (artist) {
+      return getFromApi(`artists/${artist.id}/top-tracks`, {country: 'us'})
+        .then(topTracks => {
+          artist.tracks = topTracks.tracks
+          return artist
+        }, handleError)
     })
-  return emitter
+  return Promise.all(promises).then(() => mainArtist, handleError)
 }
 
-const getRelatedArtists = (artist, res) => {
-  const relatedReq =
-    getFromApi(`artists/${artist.id}/related-artists`)
-
-  relatedReq.on('end', relatedArtists => {
-    artist.related = relatedArtists.artists
-    res.json(artist)
-  })
-  relatedReq.on('error', () => {
-    res.sendStatus(404)
-  })
+const sendResults = (res) => {
+  return artists => res.json(artists)
 }
 
-app.use(express.static('public'))
+const handleError = () => {
+  return err => console.log(err)
+}
+
+// Routes
+
+app.use(express.static(path.join(__dirname, '../public')));
 
 app.get('/search/:name', (req, res) => {
-  const searchReq = getFromApi('search', {
-    q: req.params.name,
-    limit: 1,
-    type: 'artist'
-  })
-
-  searchReq.on('end', item => {
-    const artist = item.artists.items[0]
-    getRelatedArtists(artist, res)
-  })
-  searchReq.on('error', code => {
-    res.sendStatus(code)
-  })
+  getArtist(req.params.name)
+    .then(getRelatedArtists)
+    .then(getTopTracks)
+    .then(sendResults(res))
+    .catch(handleError)
 })
 
 app.listen(8000)
